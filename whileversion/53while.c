a89f3c1e21c5a8680d22e9f8c0975402a9b53135
willwinworld <zhangzhouit@163.com>
2016-08-18 23:59:30 +0800

    to be continued

---
+++ whileversion/53while.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n, triangularNumber;
+	printf("TABLE OF TRIANGULAR NUMBERS\n\n");
+	printf("  n   Sum from 1 to n\n");
+	printf("---   ---------------\n");
+	
+	triangularNumber = 0;
+	
+	n = 1;
+	while(n <= 10)
+	{
+		triangularNumber += n;
+		printf(" %2i       %i\n", n,  triangularNumber);
+		n += 1;
+	}
+	
+	return 0;
+}
