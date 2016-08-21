0cd6cd2f40f12fd97fcbc9ad7cbd13895015d7b6
willwinworld <zhangzhouit@163.com>
2016-08-21 17:49:15 +0800

    to be continued

--- chapter6/68.c
+++ chapter6/68.c
@@ -5,7 +5,7 @@ int main(void)
 	char operator;
 	
 	printf("Type in your expression.\n");
-	scanf("%f %c%f", &value1, &operator, &value2);
+	scanf("%f %c %f", &value1, &operator, &value2);
 	
 	if (operator == '+')
 		printf("%.2f\n", value1 + value2);
