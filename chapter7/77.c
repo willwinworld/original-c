19454e152d9780a1f865469e4813fbe2039ba731
willwinworld <zhangzhouit@163.com>
2016-09-01 23:52:45 +0800

    to be continued

---
+++ chapter7/77.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main(void)
+{
+	const char baseDigits[16] = {
+		'0', '1', '2', '3', '4', '5', '6', '7',
+		'8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+	};
+	int Number, base;
+	
+}
