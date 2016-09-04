ae68c32f1b41e603390e2cda45b25e97117adeb4
willwinworld <zhangzhouit@163.com>
2016-09-04 22:56:01 +0800

    to be continued

--- chapter7/77.c
+++ chapter7/77.c
@@ -6,6 +6,29 @@ int main(void)
 		'0', '1', '2', '3', '4', '5', '6', '7',
 		'8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
 	};
-	int Number, base;
+	int convertedNumber[64];
+	long int numberToConvert;
+	int nextDigit, base, index = 0;
 	
+	printf("Number to be converted? ");
+	scanf("%ld", &numberToConvert);
+	printf("Base? ");
+	scanf("%i", &base);
+	
+	do
+	{
+		convertedNumber[index] = numberToConvert % base;
+		++index;
+		numberToConvert /= base;
+	}
+	while (numberToConvert != 0);
+	
+	for (--index; index >= 0; --index) // reverse
+	{
+		printf("%c", baseDigits[convertedNumber[index]]);
+	}
+	
+	printf("\n");
+	return 0;
 }
+
---
+++ chapter7/reverse.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main(void)
+{
+	int index = 4;
+	
+	for (index--; index >= 0; index--)
+		printf("%i", index);
+}
+
