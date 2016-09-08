4416b20256524e4fbf7df661a6ebbde7fc24ed9f
willwinworld <zhangzhouit@163.com>
2016-09-08 23:59:30 +0800

    to be continued

--- chapter7/ex7.c
+++ chapter7/ex7.c
@@ -1,8 +1,28 @@
 # include <stdio.h>
 
-int main(void)
-{
-	int P[i], n;
-	
-	
-}
+int main (void)  
+{  
+        int  P[151], i, j;  
+        int  n = 150;  
+  
+        for (i = 2; i <= n; ++i)  
+                P[i] = 0;  
+  
+        i = 2;  
+  
+        while (i <= n) {  
+                if (P[i] == 0)  
+                        printf ("%i  ", i);  
+  
+                j = 1;  
+  
+                while (i * j <= n) {  
+                        P[i * j] = 1;  // filter
+                        ++j;  
+                }  
+  
+                ++i;  
+        }  
+  
+        return 0;  
+}  
