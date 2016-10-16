bc7a169e7755257654a5342f1b2c3d9b8bda44f6
willwinworld <zhangzhouit@163.com>
2016-10-16 21:49:23 +0800

    to be contiuned

--- chapter9/99.c
+++ chapter9/99.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-struct entry
+struct entry  // entry是记录的意思 
 {
 	char word[15];
 	char definition[50];
@@ -11,7 +11,7 @@ bool equalStrings(const char s1[], const char s2[])
 {
 	int i = 0;
 	bool areEqual;
-	
+      	
 	while(s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 		++i;
 	
