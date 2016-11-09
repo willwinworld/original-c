235074a793bd336383df44f3fce9548ce32b7df7
willwinworld <zhangzhouit@163.com>
2016-11-09 23:57:42 +0800

    to be continued

--- chapter10/ex2.c
+++ chapter10/ex2.c
@@ -8,8 +8,8 @@ struct entry
 
 void insertEntry(struct entry *insertion, struct entry *previous)
 {
-	insertion->next = previous->next;
-	previous->next = insertion;
+	insertion->next = previous->next;  // 假设previous指向n2,则previous->next就代表n2的next指针，而n2的next指针之前是指向n3的，所以赋值以后就相当于告诉电脑，插入的数据结构的next指针指向n3了 
+	previous->next = insertion;  // 而previous->next即指n2的next指针是指向新插入的数据结构的 
 }
 
 void printEntries(struct entry *ptr)
@@ -27,7 +27,7 @@ int main(void)
 	struct entry n2 = {.value=2, .next=&n3};
 	struct entry n1 = {.value=1, .next=&n2};
 	listPtr = &n1;
-	struct entry x = {.value = 9};
+	struct entry x = {.value = 9};  // 竟然没有指定指针 
 	
 	printEntries(listPtr);
 	insertEntry(&x, &n2);
