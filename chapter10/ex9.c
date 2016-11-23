6b6a1e19dfcff9bf310dc7a3ee27ecd06303180b
willwinworld <zhangzhouit@163.com>
2016-11-23 21:52:45 +0800

    to be continued

--- chapter10/ex9.c
+++ chapter10/ex9.c
@@ -1,32 +1,77 @@
 #include <stdio.h>
+#include <stdbool.h>
 /*Post-increment(++) has higher precedence  */
-void readLine(char buffer[])
+bool alphabetic(const char c)
 {
-	char character;
-	int i = 0;
-	
-	do
-	{
-		character = getchar();
-		buffer[i] = character;
-		++i;
-	}
-	while(character != '\n');
-	
-	buffer[i - 1] = '\0';
+	if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return true;
+	else
+		return false;
 }
 
-void readLine_pointer_version(char *ptr)
+void readLine(char *ptr)
 {
 	char character;
-	ptr = &character;
 	
 	do
 	{
 		character = getchar();
-		++ptr;
+		*ptr++ = character;  // 已经取出来是'\n',指针所指向的那个地址所间接引用的值也被赋为'\0' 
 	}
-	while(character != '\n');
+	while(character != '\n');  // 判断character == '\0',跳出循环 
 	
-	*ptr = '\0';
+	*ptr-- = '\0'; 
+}
+
+int countWords(const char string[])
+{
+	int i, wordCount = 0;
+	bool lookingForward = true;
+	
+	for(i = 0; string[i] != '\0'; i++)
+	{
+		if(alphabetic(string[i]))
+		{
+			if(lookingForward)
+			{
+				wordCount++;
+				lookingForward = false;  //  lookingForwarc相当于一个全局变量，在for里面是会不断变化的 
+			}
+		}
+		else
+		{
+			lookingForward = true;
+		}
+	}
+	
+	return wordCount;
+}
+
+int main(void)
+{
+	char text[81];
+	int totalWords = 0;
+	int countWords(const char string[]);
+	void readLine(char *ptr);
+	bool endOfText = false;
+	
+	printf("Type in your text.\n");
+	printf("When you are done, press 'RETURN'.\n\n");
+	
+	while(!endOfText)
+	{   
+	    readLine(text);
+		if(text[0] == '\0')
+		{
+			endOfText = true;
+		}
+		else
+		{
+			totalWords += countWords(text);
+		}
+	}
+	
+	printf("\nThere are %i words in the above text.\n", totalWords);
+	
+	return 0;
 }
