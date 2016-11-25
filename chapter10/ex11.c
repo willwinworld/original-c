6102784d17b0b6cb2da58375e315c38b9717e1f6
willwinworld <zhangzhouit@163.com>
2016-11-25 21:44:52 +0800

    to be contiuned

--- chapter10/ex11.c
+++ chapter10/ex11.c
@@ -1,38 +1,72 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 struct date
 {
 	int month;
 	int day;
-	int year;	
+	int year;
 };
 
-struct date dateUpdate(struct date *today)
+void dateUpdate(struct date *today)
 {
-	struct date tomorrow;
-	today = &date;
 	int numberOfDays(struct date d);
 	
-	if(today->day != numberOfDays(today))
+	if(today->day != numberOfDays(*today))
 	{
-		tomorrow.day = today->day + 1;
-		tomorrow.month = today->month;
-		tomorrow.year = today->year;
+		today->day++;
 	}
 	else if(today->month == 12)
 	{
-		tomorrow.day = 1;
-		tomorrow.month = 1;
-		tomorrow.year = today.year + 1;
+		today->day = 1;
+		today->month = 1;
+		today->year++;
 	}
 	else
 	{
-		tomorrow.day = 1
-		tomorrow.month += today->month + 1;
-		tomorrow.year = today->year;
+		today->day = 1;
+		today->month++;
 	}
-	
-	return tomorrow;
 }
 
+int numberOfDays(struct date d)
+{
+	int days;
+	bool isLeapYear(struct date d);
+	
+	const int daysPerMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	if(isLeapYear(d) == true && d.month == 2)
+		days = 29;
+	else
+		days = daysPerMonth[d.month - 1];
+		
+	return days;
+}
 
+bool isLeapYear(struct date d)
+{
+	bool leapYearFlag;
+	
+	if((d.year % 4 == 0 && d.year % 100 != 0) || d.year % 400 == 0)
+		leapYearFlag = true;
+	else
+		leapYearFlag = false;
+		
+	return leapYearFlag;
+}
+
+int main(void)
+{
+	void dateUpdate(struct date *today);
+	struct date thisDay;
+	
+	printf("Enter today's date (mm dd yyyy): ");
+	scanf("%i%i%i", &thisDay.month, &thisDay.day, &thisDay.year);
+	
+	dateUpdate(&thisDay);
+	
+	printf("Tomorrow's date is %i/%i/%.2i.\n", thisDay.month, thisDay.day, thisDay.year % 100);
+	
+	return 0;
+}
